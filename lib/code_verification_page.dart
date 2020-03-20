de64fd1177479274ea779fa95f3ad8579a4c05e5
noeli <your_email@example.com>
2020-03-20 19:30:38 +0100

    beginning of verification view logic

--- lib/code_verification_page.dart
+++ lib/code_verification_page.dart
@@ -23,6 +23,15 @@ class CodeVerificationPage extends StatefulWidget {
 
 class _CodeVerificationPageState extends State<CodeVerificationPage> {
 
+  TextEditingController controllerInput1=new TextEditingController();
+  TextEditingController controllerInput2=new TextEditingController();
+  TextEditingController controllerInput3=new TextEditingController();
+  TextEditingController controllerInput4=new TextEditingController();
+  TextEditingController controllerInput5=new TextEditingController();
+  String code;
+  
+
+
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
@@ -45,29 +54,226 @@ class _CodeVerificationPageState extends State<CodeVerificationPage> {
           onPressed: () {},
         )
         
-        // title: Text(
-        //   'Verification Code',
-        //   style: TextStyle(
-        //     fontSize: 35,
-        //     color: Color.fromRGBO(250, 218, 0, 1),
-        //     fontWeight: FontWeight.bold,
-        //     shadows: <Shadow>[
-        //         Shadow(
-        //           blurRadius: 20,
-        //           color: Color.fromRGBO(0, 0, 50, 1),
-        //           offset: Offset(0, 13.0)
-        //         )
-        //     ]
-        //   ),
-        // ),
       ),
       body: Center(
         child: Column(
           children: <Widget>[
-            
+            showHeaderText(),
+            SizedBox(height: 40),
+            Container(
+              height: MediaQuery.of(context).size.height*0.1,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  CodeInput(
+                    controller: controllerInput1,
+                  ),
+                  CodeInput(
+                    controller: controllerInput2,
+                  ),
+                  CodeInput(
+                    controller: controllerInput3,
+                  ),
+                  CodeInput(
+                    controller: controllerInput4,
+                  ),
+                  CodeInput(
+                    controller: controllerInput5,
+                  )
+                ],
+              ),
+            ),
+            FlatButton(
+              child: Text(
+                'Resend Code',
+                style: TextStyle(
+                  fontWeight:FontWeight.bold,
+                  fontSize: 17,
+                ),
+              ),
+              onPressed: () {},
+            ),
+            SizedBox(height: MediaQuery.of(context).size.height*0.08),
+            Container(
+              height: MediaQuery.of(context).size.height*0.5,
+              width: MediaQuery.of(context).size.width*0.7,
+              child: showPadNumbers()
+            )
           ],
         )
       )// This trailing comma makes auto-formatting nicer for build methods.
+    );
+
+    
+  }
+
+  tapPadNumber(String text){
+    controllerInput1.text=text;
+  }
+
+  Widget showPadNumbers(){
+    return Column(
+      children: <Widget>[
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            PadNumber(
+              number:1,
+              onPressed: () {tapPadNumber('1');},
+            ),
+            PadNumber(
+              number:2,
+              onPressed: () {tapPadNumber('2');},
+            ),
+            PadNumber(
+              number:3,
+              onPressed: () {tapPadNumber('3');},
+            )
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            PadNumber(
+              number:4,
+              onPressed: () {tapPadNumber('4');},
+            ),
+            PadNumber(
+              number:5,
+              onPressed: () {tapPadNumber('5');},
+            ),
+            PadNumber(
+              number:6,
+              onPressed: () {tapPadNumber('6');},
+            )
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            PadNumber(
+              number:7,
+              onPressed: () {tapPadNumber('7');},
+            ),
+            PadNumber(
+              number:8,
+              onPressed: () {tapPadNumber('8');},
+            ),
+            PadNumber(
+              number:9,
+              onPressed: () {tapPadNumber('9');},
+            )
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            SizedBox(height: 60,width: 60),
+            PadNumber(
+              number:0,
+              onPressed: () {tapPadNumber('0');},
+            ),
+            Container(
+              width: 60,
+              height: 60,
+              decoration: BoxDecoration(
+                color: Color.fromRGBO(27, 34, 50, 0.8),
+                borderRadius: BorderRadius.circular(50)
+              ),
+              child: MaterialButton(
+                  child: Icon(
+                    Icons.backspace,
+                    color: Color.fromRGBO(250, 218, 0, 1),
+                  ), 
+                  onPressed: () {},
+              ),
+            )
+          ],
+        )
+      ],
+    );
+  }
+
+  Widget showHeaderText(){
+    return  Center(
+              child: Text(
+                'Verification Code',
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight:FontWeight.bold,
+                  color:Color.fromRGBO(27, 34, 50, 1),
+                  // shadows: <Shadow>[
+                  //   Shadow(blurRadius: 40,color: Color.fromRGBO(27, 34, 50, 1),offset: Offset(0,10))
+                  // ]
+                ),
+              ),
+            );
+  }
+
+  
+}
+
+class CodeInput extends StatelessWidget {
+  // This widget is the root of your application.
+
+  final TextEditingController controller; 
+  CodeInput({this.controller});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width*0.15,
+      child: TextField(
+        controller: controller,
+        enabled: false,
+        obscureText: false,
+        textAlign: TextAlign.center,
+        style: TextStyle(
+          color: Color.fromRGBO(250, 218, 0, 1),
+          fontSize: 40,
+          fontWeight: FontWeight.bold,
+        ),
+        decoration: InputDecoration(
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+            borderSide: BorderSide.none
+          ),
+          contentPadding: EdgeInsets.all(10),
+          filled: true,
+          fillColor: Color.fromRGBO(27, 34, 50, 0.5),
+        ),
+      ),
+    );
+  }
+}
+
+class PadNumber extends StatelessWidget {
+  // This widget is the root of your application.
+
+  final int number; 
+  final Function() onPressed;
+  PadNumber({this.number,this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width*0.15,
+      height: MediaQuery.of(context).size.width*0.15,
+      margin: EdgeInsets.all(4),
+      decoration: BoxDecoration(
+        color: Color.fromRGBO(27, 34, 50, 0.8),
+        borderRadius: BorderRadius.circular(50)
+      ),
+      child: MaterialButton(
+        child: Text(
+          '$number',
+          style:TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 30,
+            color: Color.fromRGBO(250, 218, 0, 1),
+          )
+        ), onPressed: onPressed,
+      ),
     );
   }
 }
